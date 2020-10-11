6d27583383430b5ce8a6662a3f1c4c45d8e2ed13
Marcos Henrique <marcoshenriqueh393@gmail.com>
2020-10-11 17:05:58 -0300

    chore: config and add eslint/folders

---
+++ __mocks__/data/cofes.ts
@@ -0,0 +1,22 @@
+
+
+export const Cofes: Array<Object> = [
+   {
+      name: 'Caramel Ribbon',
+      value: '$ 39.00',
+      imagePath: '../assets/caramel_ribbon.png',
+      description: `Caramel Ribbon meets, milk and ice for a rendezvous in the blender,
+      while whipped cream and buttery caramel sauce layer the love on top. To change things
+      up, try affogato-style with a hot espresso shot poured right ovr the top.
+      `
+   },
+   {
+   name: 'Apple Ribbon',
+   value: '$ 39.00',
+   imagePath: '../assets/apple_ribbon.png',
+   description: `Apple Ribbon meets, milk and ice for a rendezvous in the blender,
+   while whipped cream and buttery apple sauce layer the love on top. To change things
+   up, try affogato-style with a hot espresso shot poured right ovr the top.
+   `
+}
+];
