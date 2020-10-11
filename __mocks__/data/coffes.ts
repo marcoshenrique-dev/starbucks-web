e3a238db1c5404a8edb696c896182758545141d9
Marcos Henrique <marcoshenriqueh393@gmail.com>
2020-10-11 18:40:41 -0300

    chore: add eslint and config final for project

--- __mocks__/data/coffes.ts
+++ __mocks__/data/coffes.ts
@@ -11,12 +11,39 @@ export const Coffes: Array<Object> = [
       `
    },
    {
-   name: 'Apple Ribbon',
+   name: 'Passion Ribbon',
    value: '$ 39.00',
-   imagePath: '../assets/apple_ribbon.png',
-   description: `Apple Ribbon meets, milk and ice for a rendezvous in the blender,
-   while whipped cream and buttery apple sauce layer the love on top. To change things
+   imagePath: '../assets/passion_ribbon.png',
+   description: `Passion Ribbon meets, milk and ice for a rendezvous in the blender,
+   while whipped cream and buttery Passion sauce layer the love on top. To change things
    up, try affogato-style with a hot espresso shot poured right ovr the top.
    `
-}
+   },
+   {
+   name: 'Chocolate Ribbon',
+   value: '$ 39.00',
+   imagePath: '../assets/chocolate_ribbon.png',
+   description: `Chocolate Ribbon meets, milk and ice for a rendezvous in the blender,
+   while whipped cream and buttery Chocolate sauce layer the love on top. To change things
+   up, try affogato-style with a hot espresso shot poured right ovr the top.
+   `
+   },
+   {
+      name: 'Vanilla Ribbon',
+      value: '$ 39.00',
+      imagePath: '../assets/vanilla_ribbon.png',
+      description: `Vanilla Ribbon meets, milk and ice for a rendezvous in the blender,
+      while whipped cream and buttery vanilla sauce layer the love on top. To change things
+      up, try affogato-style with a hot espresso shot poured right ovr the top.
+      `
+   },
+   {
+      name: 'Special Ribbon',
+      value: '$ 39.00',
+      imagePath: '../assets/special_ribbon.png',
+      description: `Special Ribbon meets, milk and ice for a rendezvous in the blender,
+      while whipped cream and buttery Special sauce layer the love on top. To change things
+      up, try affogato-style with a hot espresso shot poured right ovr the top.
+      `
+   }
 ];
